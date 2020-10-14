0d5875dde27e869ea16eaf597c5508b04006897c
rgiuffre90 <rgiuffre90@gmail.com>
2020-10-14 12:35:12 -0500

    module  3 assignment

--- twittoff/app.py
+++ twittoff/app.py
@@ -1,9 +1,10 @@
 """Main app/routing file for Twitoff"""
 
-from flask import Flask, render_template
-from .models import DB, User,
+from flask import Flask, render_template, request
+from .models import DB, User
 from .twitter import insert_example_users
 from os import getenv
+from .predict import predict_user
 
 def create_app():
     """Creates and Configures a Flask application"""
@@ -16,14 +17,28 @@ def create_app():
     @app.route('/')
     def root():
         return render_template("base.html", title="Home", user=User.query.all())
-
-    
+   
     @app.route('/update')
     def update():
         insert_example_users()
         return render_template('base.html',  title="Home", users=User.query.all())
     
+    @app.route('/compare')
+    def compare():
+        user0, user1 = sorted([request.values['user1'],
+                               request.values['user2']]
+                               )
+        if user0 == user1:
+            message = "Cannot compare users to themselves!"
 
+        else:
+            predicition = predict_user(user0, user1, request.values['tweet_text'])
+            message = "{} is more likely to be said {} than {}".format(
+                request.values['tweet_text'], user1 if predicition else user0,
+                user0 if predicition else user1)
+        return render_template('predicition.html', title= 'Predicition', message = message)
+    
+    
     @app.route('/')
     def reset():
         DB.drop_all()
--- twittoff/twitter.py
+++ twittoff/twitter.py
@@ -4,6 +4,7 @@ import spacy
 from .models import DB, Tweet, User
 from os import getenv
 
+
 TWITTER_API_KEY = getenv('TWITTER_API_KEY')
 TWITTER_API_KEY_SECRET = getenv('TWITTER_API_KEY_SECRET')
 TWITTER_AUTH =  tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_KEY_SECRET)
@@ -16,27 +17,27 @@ def vectorize_tweet(tweet_text):
 
 def add_or_update_user(username):
     try:
-"""Allows us to add/update users to our DB"""
-    twitter_user = TWITTER.get_user(username)
-    db_user = (User.query.get(twitter_user.id)) or User(id=twitter_user.id, name=username)
-    DB.session.add(db_user)
+        """Allows us to add/update users to our DB"""
+        twitter_user = TWITTER.get_user(username)
+        db_user = (User.query.get(twitter_user.id)) or User(id=twitter_user.id, name=username)
+        DB.session.add(db_user)
 
-    tweets = twitter_user.timeline(
+        tweets = twitter_user.timeline(
         count=200, exclude_replies=True, 
         include_rts=False, tweet_mode='extended'
     )
 
-    if tweets:
-        db_user.newest_tweet_id = tweets[0].id
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
 
-    for tweet in tweets:
-        vectorize_tweet = vectorize_tweet(tweet.full_text)
-        db_tweet = Tweet(
-            id=tweet.id, text=tweet.full_text,
-            vect = vectorize_tweet
-        )
-        db_user.tweets.append(db_tweet)
-        DB.session.add(db_tweet)
+        for tweet in tweets:
+            vectorize_tweet = vectorize_tweet(tweet.full_text)
+            db_tweet = Tweet(
+                id=tweet.id, text=tweet.full_text,
+                vect = vectorize_tweet
+            )
+            db_user.tweets.append(db_tweet)
+            DB.session.add(db_tweet)
 
     except Exception as e:
         print('Error Processing: {}: {}'.format(username, e))
